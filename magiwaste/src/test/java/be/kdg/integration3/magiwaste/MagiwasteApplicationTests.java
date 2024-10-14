6c79384bc651eccb1759b7a111a53bb1f3ceb3db
mj <mohamed-jan.jalloh@student.kdg.be>
2024-10-14 14:51:46 +0200

    spring boot application setup ;)

---
+++ magiwaste/src/test/java/be/kdg/integration3/magiwaste/MagiwasteApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kdg.integration3.magiwaste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagiwasteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
