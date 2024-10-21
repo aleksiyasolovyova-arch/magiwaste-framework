aa1a27fd71c2442bffbcb2b389c71fc3a86c45af
Szymon Walkusz <szymon.walkusz@student.kdg.be>
2024-10-21 11:27:16 +0200

    actual initial commit

---
+++ src/main/java/be/kdg/magiwastebackend/MagiwasteBackendApplication.java
@@ -0,0 +1,13 @@
+package be.kdg.magiwastebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagiwasteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagiwasteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/kdg/magiwastebackend/MagiwasteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kdg.magiwastebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagiwasteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
