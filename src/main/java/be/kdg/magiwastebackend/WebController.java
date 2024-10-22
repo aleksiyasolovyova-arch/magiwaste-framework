a37223507b3c5249f4f087e9473b74e808c928a0
Szymon Walkusz <szymon.walkusz@student.kdg.be>
2024-10-23 00:18:10 +0200

    pushed index.html

---
+++ src/main/java/be/kdg/magiwastebackend/WebController.java
@@ -0,0 +1,13 @@
+package be.kdg.magiwastebackend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String getIndex() {
+        return "index";
+    }
+}
