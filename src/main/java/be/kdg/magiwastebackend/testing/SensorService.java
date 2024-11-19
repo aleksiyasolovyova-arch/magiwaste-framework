059d1f47e16a0256f67c7466e258f1f28ef5fc6e
kdg-sh <storm.haazen@student.kdg.be>
2024-11-19 17:27:02 +0100

    removed the testing directory and its files

--- src/main/java/be/kdg/magiwastebackend/testing/SensorService.java
+++
@@ -1,9 +0,0 @@
-package be.kdg.magiwastebackend.testing;
-
-import java.util.List;
-
-public interface SensorService {
-    public void addData(SensorData sensorData);
-
-    public List<SensorData> getSensorData();
-}
