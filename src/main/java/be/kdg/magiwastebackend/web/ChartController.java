de39b501af2ad127bfafe2ba6236d10ee30e3974
mj <mohamed-jan.jalloh@student.kdg.be>
2024-12-11 18:55:45 +0100

    restructuing dashboard layout!

--- src/main/java/be/kdg/magiwastebackend/web/ChartController.java
+++ src/main/java/be/kdg/magiwastebackend/web/ChartController.java
@@ -29,11 +29,12 @@ public class ChartController {
     @GetMapping("/charts")
     public String getChartData(@RequestParam(required = false, defaultValue = "1") Long binId, Model model) {
         List<WasteBinEvent> events = wasteBinEventService.findAll();
+
         Map<Long, List<WasteBinEvent>> eventsByBin = events.stream()
                 .collect(Collectors.groupingBy(event -> event.getBin().getId()))
                 .entrySet()
                 .stream()
-                .limit(25)
+                .limit(10)
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
         List<WasteBinEvent> oneBinEvent = events.stream()
@@ -42,11 +43,21 @@ public class ChartController {
 
         List<WeatherEvent> weatherEvents = weatherEventService.findAll();
 
+        long totalBins = events.size();
+        long totalTilts = events.stream().filter(
+                event -> event.isTiltState()
+        ).count();
+
+        long totalWasteReceived = events.stream().filter(event -> event.getWasteReceived() == true).count();
+
         model.addAttribute("events", events);
         model.addAttribute("weatherEvents", weatherEvents);
         model.addAttribute("eventsByBin", eventsByBin);
         model.addAttribute("oneBinEvent", oneBinEvent);
         model.addAttribute("binId", binId);
+        model.addAttribute("totalBins", totalBins);
+        model.addAttribute("totalTilts", totalTilts);
+        model.addAttribute("totalWasteReceived", totalWasteReceived);
         return "charts";
 
     }
