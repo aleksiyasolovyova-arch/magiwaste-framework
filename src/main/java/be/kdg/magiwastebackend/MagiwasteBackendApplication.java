00378f7e6beffe7795442e8536d34fa0568861cd
mj <mohamed-jan.jalloh@student.kdg.be>
2024-12-18 21:56:36 +0100

    mqtt implementation finished.

--- src/main/java/be/kdg/magiwastebackend/MagiwasteBackendApplication.java
+++ src/main/java/be/kdg/magiwastebackend/MagiwasteBackendApplication.java
@@ -1,8 +1,22 @@
 package be.kdg.magiwastebackend;
 
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
 
 @SpringBootApplication
 public class MagiwasteBackendApplication implements CommandLineRunner {
@@ -10,7 +24,7 @@ public class MagiwasteBackendApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(MagiwasteBackendApplication.class, args);
     }
-
+    
     public void run(String... args) throws Exception {
 
     }
